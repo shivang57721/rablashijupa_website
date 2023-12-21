4e93529cf9bdb1098ebc79ca469ed75d11a191a2
Paoloc18 <117180493+Paoloc18@users.noreply.github.com>
2023-12-21 22:02:46 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,21 @@
-document.getElementById('map_selection').addEventListener('change', function() {
+document.getElementById('increase_selection').addEventListener('change', function() {
     var selectedValue = this.value;
     
-    document.getElementById('image1').style.display = 'none';
-    document.getElementById('image2').style.display = 'none';
+    document.getElementById('biology').style.display = 'none';
+    document.getElementById('books').style.display = 'none';
+    document.getElementById('videogames').style.display = 'none';
+    document.getElementById('internetculture').style.display = 'none';
+    document.getElementById('histandgov').style.display = 'none';
 
     if (selectedValue === 'map1') {
-        document.getElementById('image1').style.display = 'block';
+        document.getElementById('biology').style.display = 'block';
     } else if (selectedValue === 'map2') {
-        document.getElementById('image2').style.display = 'block';
+        document.getElementById('books').style.display = 'block';
+    } else if (selectedValue === 'map3') {
+        document.getElementById('videogames').style.display = 'block';
+    }else if (selectedValue === 'map4') {
+        document.getElementById('internetculture').style.display = 'block';
+    }else if (selectedValue === 'map5') {
+        document.getElementById('histandgov').style.display = 'block';
     }
 });
