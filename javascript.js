eb30a692463dc171cf1d1ed0b1ac7fe54ebbda14
Paoloc18 <117180493+Paoloc18@users.noreply.github.com>
2023-12-21 22:08:25 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,13 +9,34 @@ document.getElementById('increase_selection').addEventListener('change', functio
 
     if (selectedValue === 'map1') {
         document.getElementById('biology').style.display = 'block';
+        document.getElementById('books').style.display = 'none';
+        document.getElementById('videogames').style.display = 'none';
+        document.getElementById('internetculture').style.display = 'none';
+        document.getElementById('histandgov').style.display = 'none';
     } else if (selectedValue === 'map2') {
+        document.getElementById('biology').style.display = 'none';
         document.getElementById('books').style.display = 'block';
+        document.getElementById('videogames').style.display = 'none';
+        document.getElementById('internetculture').style.display = 'none';
+        document.getElementById('histandgov').style.display = 'none';
+        
     } else if (selectedValue === 'map3') {
+        document.getElementById('biology').style.display = 'none';
+        document.getElementById('books').style.display = 'none';
         document.getElementById('videogames').style.display = 'block';
+        document.getElementById('internetculture').style.display = 'none';
+        document.getElementById('histandgov').style.display = 'none';
     }else if (selectedValue === 'map4') {
+        document.getElementById('biology').style.display = 'none';
+        document.getElementById('books').style.display = 'none';
+        document.getElementById('videogames').style.display = 'none';
         document.getElementById('internetculture').style.display = 'block';
+        document.getElementById('histandgov').style.display = 'none';
     }else if (selectedValue === 'map5') {
+        document.getElementById('biology').style.display = 'none';
+        document.getElementById('books').style.display = 'none';
+        document.getElementById('videogames').style.display = 'none';
+        document.getElementById('internetculture').style.display = 'none';
         document.getElementById('histandgov').style.display = 'block';
     }
 });
