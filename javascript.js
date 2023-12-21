2a6dc60ee8a3bd35e3a88f387037c39a107585a0
Paoloc18 <117180493+Paoloc18@users.noreply.github.com>
2023-12-21 22:22:17 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -40,3 +40,35 @@ document.getElementById('increase_selection').addEventListener('change', functio
         document.getElementById('histandgov').style.display = 'block';
     }
 });
+
+document.getElementById('decrease_selection').addEventListener('change', function() {
+    var selectedValue = this.value;
+    
+    document.getElementById('sports').style.display = 'none';
+    document.getElementById('education').style.display = 'none';
+    document.getElementById('engineering').style.display = 'none';
+    document.getElementById('fashion').style.display = 'none';
+
+    if (selectedValue === 'mp1') {
+        document.getElementById('sports').style.display = 'block';
+        document.getElementById('education').style.display = 'none';
+        document.getElementById('engineering').style.display = 'none';
+        document.getElementById('fashion').style.display = 'none';
+    } else if (selectedValue === 'mp2') {
+        document.getElementById('sports').style.display = 'none';
+        document.getElementById('education').style.display = 'block';
+        document.getElementById('engineering').style.display = 'none';
+        document.getElementById('fashion').style.display = 'none';
+        
+    } else if (selectedValue === 'mp3') {
+        document.getElementById('sports').style.display = 'none';
+        document.getElementById('education').style.display = 'none';
+        document.getElementById('engineering').style.display = 'block';
+        document.getElementById('fashion').style.display = 'none';
+    }else if (selectedValue === 'mp4') {
+        document.getElementById('sports').style.display = 'none';
+        document.getElementById('education').style.display = 'none';
+        document.getElementById('engineering').style.display = 'none';
+        document.getElementById('fashion').style.display = 'block';
+    }
+});
