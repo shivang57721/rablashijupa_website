7890651c4c5daf15f4f6c1c4798785a3cb449f7d
rfuleiha <117180588+rfuleiha@users.noreply.github.com>
2023-12-21 19:20:05 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 const register_c = document.querySelector("#registration_selection_country");
 
 register_c.addEventListener('change', (event) => {
-  if (register_c.value === "Europe") {
+  if (register_c.value === "map1") {
     show_Sweden.style.display = "block";
     show_Denmark.style.display = "none";
       }
-  else if (register_c.value === "Asia") {
+  else if (register_c.value === "map2") {
     show_Sweden.style.display = "none";
     show_Denmark.style.display = "block";
       }
