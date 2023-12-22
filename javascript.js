b34dacd3b99c7c273447cb871efbefa1b5baa94e
rfuleiha <117180588+rfuleiha@users.noreply.github.com>
2023-12-22 14:25:47 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 document.getElementById('increase_selection').addEventListener('change', function() {
     var selectedValue = this.value;
+    document.getElementById('biology').style.display = 'block';
+    document.getElementById('books').style.display = 'none';
+    document.getElementById('videogames').style.display = 'none';
+    document.getElementById('internetculture').style.display = 'none';
+    document.getElementById('histandgov').style.display = 'none';
 
     if (selectedValue === 'map1') {
         document.getElementById('biology').style.display = 'block';
